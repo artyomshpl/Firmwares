25a475b8146d09af651e19a605c9c329ca723db4
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2019-02-08 12:07:29 +0300

    Add files via upload

---
+++ PROJECTSMARTHOUSE_0.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+Adafruit_BMP280 bmp; 
+
+int MD= 7;
+int AS= A0;
+
+void setup() {
+  Serial.begin(9600);
+ pinMode(MD, INPUT);
+ pinMode(AS, INPUT); 
+}
+
+void loop() {
+ //Датчик движения HC-SR-501
+ if(MoveDetector() == 1){
+  Serial.println("Move!");
+ }
+ else{
+  Serial.println("No move.");
+ }
+
+ //Датчик звука
+ Serial.println(AudioSensor());
+
+}
+bool MoveDetector(){
+  int valMD;
+
+  valMD = digitalRead(MD);
+
+  if(valMD == 1){
+    return(1);
+  }
+  else{
+    return(0);
+  }
+}
+
+ int AudioSensor(){
+  return(analogRead(AS));
+ }
+  
+
+
